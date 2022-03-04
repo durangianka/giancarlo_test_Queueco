3e124959f816e21b407d1f9f63c4533efb8523a2
durangianka <durangianka@gmail.com>
2022-03-04 11:20:20 +0000

    Update App.js

--- btcusd-test/src/App.js
+++ btcusd-test/src/App.js
@@ -4,41 +4,39 @@ const ws = new WebSocket("wss://api.gemini.com/v1/marketdata/BTCUSD?bids=true&of
 
 function App() {
 
-  const [bid, setBid] = useState({})
-  const [ask, setAsk] = useState({})
+  const [bid, setBid] = useState([{}])
+  const [ask, setAsk] = useState([{}])
 
   useEffect(() => {
     ws.addEventListener("message", (e) => {
       const data = JSON.parse(e.data)
-      // data.events.map(event => {
-      //   if (event.side === 'bid') {
-      //     setBid(event);
-      //   } else if (event.side === 'ask') {
-      //     setAsk(event);
-      //   }
-
-      // })
+      if (data.type === "update") {
+        data.events.map(event => {
+          if (event.side === 'bid') {
+            setBid(btc => [...btc, event])
+          } else if (event.side === 'ask') {
+            setAsk(ask => [...ask, event])
+          }
+        })
+      }
       console.log(data)
     })
   }, []);
 
-
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center bg-slate-700 text-white">
-      <div>
-        {/* {bid &&
-          (
-            <p>{bid.side}</p>
-          )
-        }
-      </div>
-      <div>
-        {ask &&
-          (
-            <p>{ask.side}</p>
-          )
-        } */}
-      </div>
+      {bid[1] &&
+        (
+          <>
+            <div>
+              <p>{bid[bid.length - 1].price} {bid[bid.length - 1].remaining} - {ask[ask.length - 1].price} {ask[ask.length - 1].remaining}</p>
+            </div>
+            <div>
+              <p>{bid[bid.length - 2].price} {bid[bid.length - 2].remaining} - {ask[ask.length - 2].price} {ask[ask.length - 2].remaining}</p>
+            </div>
+          </>
+        )
+      }
     </div >
   );
 }
